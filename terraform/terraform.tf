6ebb7415ccd0ab40cf8a895a4074c03ac7a6a60a
AV tech <gharsajaawatamz@gmail.com>
2024-05-31 03:16:30 +0530

    terraform code

---
+++ terraform/terraform.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.51.1"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIASCOMMFCLJ2OZPV2U"
+  secret_key = "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"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
